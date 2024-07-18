a071381b032a6caaabde8e852cee12d99f7ef466
ElisabettaMirti <belmirti@gmail.com>
2024-07-18 09:44:14 +0200

    aggiustato nome Model (ora funziona)

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\movies;
 
 class PageController extends Controller
 {
     //
     public function index(){
 
-        $movies = Movie::all();
+        $movies = movies::all();
         return view('pages.home', compact('movies'));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,4 +4,19 @@
 
 @section('main-content')
 <h1>Movie List</h1>
+
+<section class="movies row">
+    @foreach ($movies as $movie)
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{ $movie->title }}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+
+    @endforeach
+</section>
+
+
+
 @endsection
