679841eec3346d184f8ad80cd80ae6dc2009e2de
ElisabettaMirti <belmirti@gmail.com>
2024-07-18 17:01:29 +0200

    inserite card con tutte le info

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,18 +3,24 @@
 @section('page-title', 'Homepage')
 
 @section('main-content')
-<h1>Movie List</h1>
+<h1 class="p-4 text-center">Movie List</h1>
 
-<section class="movies row">
-    @foreach ($movies as $movie)
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">{{ $movie->title }}</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
+
+<section class="movies container-fluid p-4">
+    <div class="row d-flex justify-content-center">
+        @foreach ($movies as $movie)
+            <div class="card col-2 m-2">
+                <div class="card-body p-4">
+                    <h5 class="card-title mb-4"><strong>{{ $movie->title }}</strong></h5>
+                    <p class="card-text">Titolo originale: {{ $movie->original_title }}</p>
+                    <p class="card-text">Paese: {{ $movie->nationality }}</p>
+                    <p class="card-text">Data d'uscita: {{ $movie->date }}</p>
+                    <p class="card-text">Voto: {{ $movie->vote }}</p>
+                </div>
+            </div>
+        @endforeach
     </div>
 
-    @endforeach
 </section>
 
 
